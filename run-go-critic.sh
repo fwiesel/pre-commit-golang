09506778b180184b78ad3dc2fef06255dcf91bdb
Fabian Wiesel <fwiesel@users.noreply.github.com>
2022-02-15 10:33:12 +0100

    Run-Go-Critic: Pass all arguments directyl
    Not all arguments are necessarily files as the script assumes.
    
    One might want to enable or disable individual filters by passing the corresponding arguments,
    ```
    args: [-disable, somefilter, -enable another]
    ```
    And passing simply all the files as arguments should do the trick.

--- run-go-critic.sh
+++ run-go-critic.sh
@@ -1,15 +1,3 @@
 #!/usr/bin/env bash
 
-failed=false
-
-for file in "$@"; do
-    # redirect stderr so that violations and summaries are properly interleaved.
-    if ! gocritic check "$file" 2>&1
-    then
-        failed=true
-    fi
-done
-
-if [[ $failed == "true" ]]; then
-    exit 1
-fi
+exec gocritic check "$@" 2>&1
